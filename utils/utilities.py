b62f5d7ecb1e02ab26648764a08737ae35175e5c
Quan <qqmquach@uwtaerloo.ca>
2023-07-02 23:13:51 -0400

    remove redundant helper

--- utils/utilities.py
+++ utils/utilities.py
@@ -10,21 +10,6 @@ def compute_accuracy(Y_hat, Y):
     accuracy = correct_count / len(Y)
     return accuracy
 
-def plot_data(X, y, ax, pos_label="y=1", neg_label="y=-1", s=80, loc='best'):
-    postive = y == 1
-    negative = y == -1
-    pos = postive.reshape(-1,) #convert to 1D array if needed
-    neg = negative.reshape(-1,) #convert to 1D array if needed
-    # print(f"pos = {pos}")
-    # Plot examples
-    ax.scatter(X[pos, 0], X[pos, 1], marker='x', s=s, c = 'red', label=pos_label)
-    ax.scatter(X[neg, 0], X[neg, 1], marker='o', s=s, label=neg_label, facecolors='none', edgecolors='green', lw=3)
-    ax.legend(loc=loc)
-
-    ax.figure.canvas.toolbar_visible = False
-    ax.figure.canvas.header_visible = False
-    ax.figure.canvas.footer_visible = False
-
 def plot_analysis(title, y_label, x_data, y_data, color, x_label="Radius"):
     plt.title(title)
     plt.xlabel(x_label)
